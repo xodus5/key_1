97b6004a3141e7ae295635c06aa6c1c39bfce150
iamhanori <s2122@e-mirim.hs.kr>
2023-04-07 02:39:31 +0900

    actionber 없애기, device 재설정

--- app/src/main/java/hataehatae8/key/MainActivity.java
+++ app/src/main/java/hataehatae8/key/MainActivity.java
@@ -1,5 +1,6 @@
 package hataehatae8.key;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 액션바 없애기
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
     }
 }

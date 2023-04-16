a80aa8be203e98f7b551a4a01f254da77b64229a
iamhanori <s2122@e-mirim.hs.kr>
2023-04-16 21:48:42 +0900

    버튼 글씨 추가

---
+++ app/src/main/java/hataehatae8/key/CafeMainActivity.java
@@ -0,0 +1,14 @@
+package hataehatae8.key;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CafeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cafe_main);
+    }
+}
---
+++ app/src/main/java/hataehatae8/key/CafeMenuActivity.java
@@ -0,0 +1,14 @@
+package hataehatae8.key;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CafeMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cafe_menu);
+    }
+}

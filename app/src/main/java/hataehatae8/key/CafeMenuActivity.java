5fda64e04325a8780aaf7dbdb367b64e0bb9f19e
iamhanori <s2122@e-mirim.hs.kr>
2023-04-20 03:05:42 +0900

    액션바 없애기, main.xml

--- app/src/main/java/hataehatae8/key/CafeMenuActivity.java
+++ app/src/main/java/hataehatae8/key/CafeMenuActivity.java
@@ -1,5 +1,6 @@
 package hataehatae8.key;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class CafeMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cafe_menu);
+
+        // 액션바 없애기
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
     }
 }

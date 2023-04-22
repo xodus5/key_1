3babb4a9d08842918ae7f6705e4a55b99fda5d3d
xodus5 <sudox913@gmail.com>
2023-04-23 01:38:35 +0900

    MainActivity 버튼 이동 구현

--- app/src/main/java/hataehatae8/key/MainActivity.java
+++ app/src/main/java/hataehatae8/key/MainActivity.java
@@ -3,7 +3,10 @@ package hataehatae8.key;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.cafe_1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CafeMainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // 액션바 없애기
         ActionBar actionBar = getSupportActionBar();

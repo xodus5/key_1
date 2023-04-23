f775cc56fe8fc35b59ece1f3e6cb5ce64dbb6228
iamhanori <s2122@e-mirim.hs.kr>
2023-04-24 00:59:26 +0900

    데이터 불러오기 테스트1

--- app/src/main/java/hataehatae8/key/CafeMenuActivity.java
+++ app/src/main/java/hataehatae8/key/CafeMenuActivity.java
@@ -1,11 +1,37 @@
 package hataehatae8.key;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class CafeMenuActivity extends AppCompatActivity {
+
+    DatabaseReference mDatabase;
+    Query myTopPostsQuery;
+    Menu menuListData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +42,59 @@ public class CafeMenuActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        // 받아온 데이터를 받을 리스트
+        ArrayList<Menu> menuList = new ArrayList<>();
+
+// 데이터 받아오기 : "menuName"이 "아이스아메리카노"인 데이터 가져오기
+        mDatabase = FirebaseDatabase.getInstance().getReference("menuTBL");
+        myTopPostsQuery = mDatabase.orderByChild("menuName").equalTo("아이스아메리카노");
+        myTopPostsQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                menuList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    menuListData = dataSnapshot.getValue(Menu.class);
+                    menuList.add(menuListData);
+                }
+                Log.w("MainActivity", "menuList = "+menuList.toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("MainActivity", "onCancelled");
+
+            }
+        });
+
     }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+
+
 }
---
+++ app/src/main/java/hataehatae8/key/Menu.java
@@ -0,0 +1,16 @@
+package hataehatae8.key;
+
+public class Menu {
+    public String menuName;
+    public int price;
+
+    public Menu() {
+    }
+
+    public Menu(String menuName, int price) {
+        this.menuName = menuName;
+        this.price = price;
+
+    }
+
+}

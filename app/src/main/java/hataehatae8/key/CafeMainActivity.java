11a958a46ee9e79934947bc65c346dc6f3a9d710
xodus5 <sudox913@gmail.com>
2023-04-23 23:04:34 +0900

    Cafe menu 이동

--- app/src/main/java/hataehatae8/key/CafeMainActivity.java
+++ app/src/main/java/hataehatae8/key/CafeMainActivity.java
@@ -3,7 +3,10 @@ package hataehatae8.key;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CafeMainActivity extends AppCompatActivity {
 
@@ -11,6 +14,16 @@ public class CafeMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cafe_main);
+
+        Button button = (Button) findViewById(R.id.c_menu1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CafeMainActivity.this, CafeMenuActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // 액션바 없애기
         ActionBar actionBar = getSupportActionBar();

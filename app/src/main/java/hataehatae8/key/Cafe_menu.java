96493a57d6e832998f912a4ab6caf80dd1694a8d
iamhanori <s2122@e-mirim.hs.kr>
2023-06-17 23:21:06 +0900

    메뉴창에서 결제하기창으로 넘어가기

--- app/src/main/java/hataehatae8/key/Cafe_menu.java
+++ app/src/main/java/hataehatae8/key/Cafe_menu.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 
 public class Cafe_menu extends AppCompatActivity {
-
-    RecyclerView MenuView;
+    Button btnCash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +25,14 @@ public class Cafe_menu extends AppCompatActivity {
             actionBar.hide();
         }
 
-        initialize();
-
-    }
-
-    private void initialize() {
-        MenuView = findViewById(R.id.MenuView);
-
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
-        MenuView.setLayoutManager(gridLayoutManager);
+        btnCash = findViewById(R.id.btn_cash);
+        btnCash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Cafe_menu.this, Public_cash.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
